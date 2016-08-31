9d7c59d48f72cd79ab4e7cc8e776062191eeafe4
JimCampagno <jimcampagno@gmail.com>
2016-08-31 19:41:36 -0400

    Take out solution

--- LayoutFun/ViewController.swift
+++ LayoutFun/ViewController.swift
@@ -30,31 +30,11 @@ class ViewController: UIViewController {
 extension ViewController {
     
     func setupTheConstraints() {
+        
+        
         // TODO: Create all the constraints (in code) for the various views
-        redView.widthAnchor.constraintEqualToAnchor(view.widthAnchor, multiplier: 0.5).active = true
-        redView.heightAnchor.constraintEqualToAnchor(redView.widthAnchor).active = true
-        redView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        redView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
         
-        orangeView.widthAnchor.constraintEqualToAnchor(redView.widthAnchor, multiplier: 0.75).active = true
-        orangeView.heightAnchor.constraintEqualToAnchor(orangeView.widthAnchor).active = true
-        orangeView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        orangeView.bottomAnchor.constraintEqualToAnchor(redView.topAnchor).active = true
         
-        yellowView.widthAnchor.constraintEqualToAnchor(orangeView.widthAnchor, multiplier: 0.75).active = true
-        yellowView.heightAnchor.constraintEqualToAnchor(yellowView.widthAnchor).active = true
-        yellowView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        yellowView.bottomAnchor.constraintEqualToAnchor(orangeView.topAnchor).active = true
-        
-        greenView.widthAnchor.constraintEqualToAnchor(yellowView.widthAnchor, multiplier: 0.75).active = true
-        greenView.heightAnchor.constraintEqualToAnchor(greenView.widthAnchor).active = true
-        greenView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        greenView.bottomAnchor.constraintEqualToAnchor(yellowView.topAnchor).active = true
-        
-        blueView.widthAnchor.constraintEqualToAnchor(greenView.widthAnchor, multiplier: 0.75).active = true
-        blueView.heightAnchor.constraintEqualToAnchor(blueView.widthAnchor).active = true
-        blueView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        blueView.bottomAnchor.constraintEqualToAnchor(greenView.topAnchor).active = true
     }
     
     func turnOffAutoResizingOnAllViews() {
