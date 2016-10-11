798d505f1ed3a7b28ef182ffb01798d4975bf633
Johann Kerr <johannkerr@gmail.com>
2016-10-11 11:45:59 -0400

    fixing solution swift 3

--- LayoutFun/ViewController.swift
+++ LayoutFun/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let redView = UIView(frame: CGRectZero)
-    let orangeView = UIView(frame: CGRectZero)
-    let yellowView = UIView(frame: CGRectZero)
-    let greenView = UIView(frame: CGRectZero)
-    let blueView = UIView(frame: CGRectZero)
+    let redView = UIView(frame: CGRect.zero)
+    let orangeView = UIView(frame: CGRect.zero)
+    let yellowView = UIView(frame: CGRect.zero)
+    let greenView = UIView(frame: CGRect.zero)
+    let blueView = UIView(frame: CGRect.zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.grayColor()
+        view.backgroundColor = UIColor.gray
         turnOffAutoResizingOnAllViews()
         setupTheConstraints()
         setupColors()
@@ -30,6 +30,31 @@ class ViewController: UIViewController {
 extension ViewController {
     
     func setupTheConstraints() {
+        
+        redView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        orangeView.widthAnchor.constraint(equalTo: redView.widthAnchor, multiplier: 0.75).isActive = true
+        orangeView.heightAnchor.constraint(equalTo: orangeView.widthAnchor).isActive = true
+        orangeView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        orangeView.bottomAnchor.constraint(equalTo: redView.topAnchor).isActive = true
+        
+        yellowView.widthAnchor.constraint(equalTo: orangeView.widthAnchor, multiplier: 0.75).isActive = true
+        yellowView.heightAnchor.constraint(equalTo: yellowView.widthAnchor).isActive = true
+        yellowView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        yellowView.bottomAnchor.constraint(equalTo: orangeView.topAnchor).isActive = true
+        
+        greenView.widthAnchor.constraint(equalTo: yellowView.widthAnchor, multiplier: 0.75).isActive = true
+        greenView.heightAnchor.constraint(equalTo: greenView.widthAnchor).isActive = true
+        greenView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        greenView.bottomAnchor.constraint(equalTo: yellowView.topAnchor).isActive = true
+        
+        blueView.widthAnchor.constraint(equalTo: greenView.widthAnchor, multiplier: 0.75).isActive = true
+        blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor).isActive = true
+        blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        blueView.bottomAnchor.constraint(equalTo: greenView.topAnchor).isActive = true
         
         
         // TODO: Create all the constraints (in code) for the various views
@@ -51,10 +76,10 @@ extension ViewController {
 extension ViewController {
     
     func setupColors() {
-        redView.backgroundColor = UIColor.redColor()
-        orangeView.backgroundColor = UIColor.orangeColor()
-        yellowView.backgroundColor = UIColor.yellowColor()
-        greenView.backgroundColor = UIColor.greenColor()
-        blueView.backgroundColor = UIColor.blueColor()
+        redView.backgroundColor = UIColor.red
+        orangeView.backgroundColor = UIColor.orange
+        yellowView.backgroundColor = UIColor.yellow
+        greenView.backgroundColor = UIColor.green
+        blueView.backgroundColor = UIColor.blue
     }
 }
